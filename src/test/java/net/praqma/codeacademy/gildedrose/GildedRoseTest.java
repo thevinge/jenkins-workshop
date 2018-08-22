be5cb6cc5c620e989478724da5b7da713315feac
JV <jving16@student.sdu.dk>
2018-08-22 13:53:01 +0200

    Test

--- src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
+++ src/test/java/net/praqma/codeacademy/gildedrose/GildedRoseTest.java
@@ -14,6 +14,7 @@ public class GildedRoseTest {
         Item[] items = new Item[] { new Item("foo", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
+        // Comment
         assertEquals("foo", app.items[0].name);
     }
 
